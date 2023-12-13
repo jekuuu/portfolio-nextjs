f17f9f5dc7d0136349dec64905e812e781bad390
Jekib <jekib.haque@gmail.com>
2023-12-13 21:04:16 +0530

    Fixed layout issues

--- app/components/sections/Hero.tsx
+++ app/components/sections/Hero.tsx
@@ -4,9 +4,9 @@ import Link from "next/link";
 
 const HeroSection = () => {
   return (
-    <section className="h-[100vh] pt-12">
+    <section className="h-[100vh] pt-12 mx-7">
       <div className="flex justify-center">
-        <div className="flex flex-col align-middle">
+        <div className="flex flex-col">
           <h1 className="text-7xl pt-40">
             Hey, I&apos;m{" "}
             <span className="text-transparent bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text  hover:from-pink-500 hover:via-red-500 hover:to-yellow-500">
@@ -17,7 +17,7 @@ const HeroSection = () => {
           <a
             href="/resume/resume.pdf"
             download={"Jekib Alam Haque Resume.pdf"}
-            className="mt-10 border p-5 shadow-lg w-48 hover:shadow-slate-500 hover:scale-105"
+            className="mt-10 border p-5 shadow-lg w-48 hover:shadow-slate-500 hover:scale-105 hover:bg-gradient-to-r hover:from-gray-700 hover:via-gray-900 hover:to-black"
           >
             Download Resume
           </a>
@@ -25,10 +25,15 @@ const HeroSection = () => {
             <Link
               href={"https://www.linkedin.com/in/jekibalamhaque/"}
               target="_blank"
+              aria-label="Jekib Alam Haque's LinkedIn Profile URL"
             >
               <FontAwesomeIcon icon={faLinkedin} className="w-5" />
             </Link>
-            <Link href={"https://github.com/jekuuu"} target="_blank">
+            <Link
+              href={"https://github.com/jekuuu"}
+              target="_blank"
+              aria-label="Jekib Alam Haque's Github Profile URL"
+            >
               <FontAwesomeIcon icon={faGithub} className="w-5" />
             </Link>
           </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import Footer from "./components/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Jekib Alam Haque",
+  title: "Jekib Alam Haque | Portfolio",
   description: "Portfolio of Jekib Alam Haque",
 };
 
