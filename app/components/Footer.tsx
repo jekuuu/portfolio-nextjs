706f9b1b683774541f596bedc3af4bddaaf5fd55
Jekib <jekib.haque@gmail.com>
2023-12-16 01:31:21 +0530

    changed favicon

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,9 +1,8 @@
 const Footer = () => {
   return (
-    // <footer className="w-full bottom-0 flex justify-center dark:bg-zinc-900 p-3 mt-5">
-    //   <p>Jekib Alam Haque | Portfolio &copy; 2024</p>
-    // </footer>
-    ""
+    <footer className="w-full flex justify-center dark:bg-zinc-900 p-3 mt-5">
+      <p>Jekib Alam Haque | Portfolio &copy; 2024</p>
+    </footer>
   );
 };
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/x-icon" href="/letter-j-square.svg" />
+      </head>
       <body className={inter.className}>
         <>
           <Navbar />
