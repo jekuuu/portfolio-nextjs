cc167a9dfd5712f5f10b5544410c221e6aff87b8
Jekib <jekib.haque@gmail.com>
2023-12-16 14:15:03 +0530

    Added Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -25,6 +27,7 @@ export default function RootLayout({
         <>
           <Navbar />
           {children}
+          <Analytics />
           <Footer />
         </>
       </body>
