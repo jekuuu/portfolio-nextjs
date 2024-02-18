f14a23e00844c75e4f579f84adbd74d5cfb7a6ec
Jekib <jekib.haque@gmail.com>
2024-02-18 16:16:03 +0530

    updated Meta description

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,8 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Jekib Alam Haque | Portfolio",
-  description: "Portfolio of Jekib Alam Haque",
+  description: `As a Senior Engineering Lead at Persistent Systems, I develop and design web applications for clients using cutting-edge technologies such as React, TypeScript. I collaborate with the US team for technical discussions and code reviews, and establish reusable components for code efficiency and future maintenance. I also implement unit tests for stability and bug reduction, and deploy the applications using Docker and Kubernetes.
+  I have over seven years of experience in web development, working with different JavaScript frameworks and libraries, such as React, Next, Redux, and Node. I have transitioned older web applications to microfrontends, developed APIs with Java Spring Boot, and overseen release management and PR validation pipelines. I have also contributed to test case creation, elevating test coverage to over 90%, and addressed performance challenges and vulnerabilities in live applications. I hold a Bachelor of Engineering in Computer Science and Engineering from Gauhati University. I am passionate about creating user-friendly, scalable, and secure web solutions that meet the needs and expectations of the clients.`,
 };
 
 export default function RootLayout({
